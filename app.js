d66879665511ad92120a1f05a547d914da0c14c3
dani cornejo <dcorgar2102@g.educaand.es>
2024-11-11 12:01:05 +0100

    Modificar app.js y  añadir pugs

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 /**
  * PROYECTO LIBROS NEPM
  */
+const express = require('express');
+const session = require('express-session');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const libreriaRouter = require('./routes/libreriaRoutes');
+
+
+require('dotenv').config({ path: './libreria/.env' });
+
+/**
+ * Crea el servidor Web
+ */
+const app = express();
+const port = process.env.SERVICE_PORT;
+;
+/**/
